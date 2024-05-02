caafe44f64a31b600f5756c2a05cbda96d76b5bb
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2024-05-02 18:34:28 +0600

    compleate module-42.2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -77,7 +78,9 @@ const LoginPage = () => {
           </form>
           <p className="text-center">Or Sign Up Using</p>
           <div className="flex justify-center mb-10 mt-2">
-            <button className="btn btn-circle ">
+            <button onClick={() => signIn("google", {
+              callbackUrl: "http://localhost:3000/dashboard"
+            })} className="btn btn-circle ">
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={50}
@@ -85,7 +88,9 @@ const LoginPage = () => {
                 alt="google logo"
               />
             </button>
-            <button className="btn btn-circle">
+            <button onClick={() => signIn("github", {
+              callbackUrl: "http://localhost:3000/dashboard"
+            })} className="btn btn-circle">
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                 width={35}
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,13 +1,18 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
-    // ...add more providers here
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
